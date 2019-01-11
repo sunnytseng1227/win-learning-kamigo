5a57419f3dcbde15598e87d1c01714b54647f5e9
sunny <sunny.ann.tseng@gmail.com>
2019-01-11 17:47:36 +0800

    no message

--- app/controllers/kamigo_controller.rb
+++ app/controllers/kamigo_controller.rb
@@ -85,8 +85,77 @@ class KamigoController < ApplicationController
                        ]
                    }
                  }
+            when "有什麼服務"
+              {
+                "type": "template",
+                "altText": "this is a carousel template",
+                "template": {
+                    "type": "carousel",
+                    "columns": [
+                        {
+                          "thumbnailImageUrl": "https://cdn2.ettoday.net/images/3826/d3826516.jpg",
+                          "imageBackgroundColor": "#FFFFFF",
+                          "title": "this is menu",
+                          "text": "description",
+                          "defaultAction": {
+                              "type": "uri",
+                              "label": "View detail",
+                              "uri": "http://example.com/page/123"
+                          },
+                          "actions": [
+                              {
+                                  "type": "postback",
+                                  "label": "Buy",
+                                  "data": "action=buy&itemid=111"
+                              },
+                              {
+                                  "type": "postback",
+                                  "label": "Add to cart",
+                                  "data": "action=add&itemid=111"
+                              },
+                              {
+                                  "type": "uri",
+                                  "label": "View detail",
+                                  "uri": "http://example.com/page/111"
+                              }
+                          ]
+                        },
+                        {
+                          "thumbnailImageUrl": "https://cdn2.ettoday.net/images/3826/c3826788.jpg",
+                          "imageBackgroundColor": "#000000",
+                          "title": "this is menu",
+                          "text": "description",
+                          "defaultAction": {
+                              "type": "uri",
+                              "label": "View detail",
+                              "uri": "http://example.com/page/222"
+                          },
+                          "actions": [
+                              {
+                                  "type": "postback",
+                                  "label": "Buy",
+                                  "data": "action=buy&itemid=222"
+                              },
+                              {
+                                  "type": "postback",
+                                  "label": "Add to cart",
+                                  "data": "action=add&itemid=222"
+                              },
+                              {
+                                  "type": "uri",
+                                  "label": "View detail",
+                                  "uri": "http://example.com/page/222"
+                              }
+                          ]
+                        }
+                    ],
+                    "imageAspectRatio": "rectangle",
+                    "imageSize": "cover"
+                }
+              }
+
             else
-                 message = {
+                 {
                 type: 'text',
                 text:  message_txt + '~'
               }
