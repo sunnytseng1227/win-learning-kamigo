e9f54716e21515127f8b2005e0aa50d6fdb8c2b4
sunny <sunny.ann.tseng@gmail.com>
2019-01-14 14:34:27 +0800

    no message

--- app/controllers/kamigo_controller.rb
+++ app/controllers/kamigo_controller.rb
@@ -53,6 +53,8 @@ class KamigoController < ApplicationController
     case postback_type
     when "Call_for_service"
       template_service(event)
+    when "more_1"
+      template_more_1(event)
     end
   end
 
@@ -84,7 +86,146 @@ class KamigoController < ApplicationController
      }
      client.reply_message(event['replyToken'], message)
   end
-
+  def template_more_1(event)
+    message =
+    {
+      "type": "bubble",
+      "header": {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [
+          {
+            "type": "text",
+            "text": "FIFA World Cup 2018",
+            "size": "xl",
+            "weight": "bold"
+          }
+        ]
+      },
+      "hero": {
+        "type": "image",
+        "url": "https://sitthi.me:3807/static/fifa.jpg",
+        "size": "full",
+        "aspectRatio": "20:13",
+        "aspectMode": "cover"
+      },
+      "body": {
+        "type": "box",
+        "layout": "vertical",
+        "spacing": "md",
+        "contents": [
+          {
+            "type": "box",
+            "layout": "horizontal",
+            "spacing": "sm",
+            "contents": [
+              {
+                "type": "text",
+                "text": "LIVE !",
+                "size": "lg",
+                "color": "#555555",
+                "weight": "bold",
+                "align": "center"
+              }
+            ]
+          },
+          {
+            "type": "button",
+            "style": "primary",
+            "action": {
+              "type": "postback",
+              "label": "Portugal  1 : 0  Morocco",
+              "displayText": "Live Report !!",
+              "data": "LIVE"
+            }
+          },
+          {
+            "type": "separator",
+            "margin": "lg"
+          },
+          {
+            "type": "box",
+            "layout": "vertical",
+            "margin": "lg",
+            "spacing": "sm",
+            "contents": [
+              {
+                "type": "box",
+                "layout": "horizontal",
+                "spacing": "sm",
+                "contents": [
+                  {
+                    "type": "button",
+                    "style": "primary",
+                    "action": {
+                      "type": "postback",
+                      "label": "Last Match",
+                      "displayText": "Last Match",
+                      "data": "LAST"
+                    }
+                  },
+                  {
+                    "type": "button",
+                    "style": "primary",
+                    "action": {
+                      "type": "postback",
+                      "label": "Next Match",
+                      "displayText": "Next Match",
+                      "data": "NEXT"
+                    }
+                  }
+                ]
+              },
+              {
+                "type": "box",
+                "layout": "horizontal",
+                "spacing": "sm",
+                "contents": [
+                  {
+                    "type": "button",
+                    "style": "primary",
+                    "action": {
+                      "type": "postback",
+                      "label": "Schedule",
+                      "displayText": "Schedule",
+                      "data": "SCHEDULE"
+                    }
+                  },
+                  {
+                    "type": "button",
+                    "style": "primary",
+                    "action": {
+                      "type": "postback",
+                      "label": "Table",
+                      "displayText": "Table",
+                      "data": "TABLE"
+                    }
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      },
+      "footer": {
+        "type": "box",
+        "layout": "vertical",
+        "contents": [
+          {
+            "type": "button",
+            "margin": "sm",
+            "action": {
+              "type": "uri",
+              "label": "View Source",
+              "uri": "https://sitthi.me:3807/downloaded/ba5f784d837540dfb40df2d531d7519c.json"
+            },
+            "style": "secondary"
+          }
+        ]
+      }
+    }
+    client.reply_message(event['replyToken'], message)
+  end
   #服務選單
   def template_service(event)
     message =  {
